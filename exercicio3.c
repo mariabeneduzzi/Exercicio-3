f7393fe1d4c8534f2fd8bcd184c2d4ebd9aa9531
mariabeneduzzi <mariaclarabeneduzzi@gmail.com>
2024-09-04 21:16:29 -0300

    Add files via upload

---
+++ exercicio3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+       printf("Digite um numero inteiro positivo: ");
+       scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Numero invalido.");
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+
+        if (i % 2 == 0) {
+            printf("Pim ");
+        }
+
+        else if (i % 3 == 0) {
+            printf("Pam ");
+        }
+        else if (i % 2 == 0 && i % 3 == 0) {
+            printf("Pim Pam ");
+        }
+         else {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
